08874381db0300353a4becb8a0bc4baa33617e20
canvasnova-web <webmaster@canvasnova.de>
2025-12-03 12:29:18 +0100

    chore: Update Facebook Pixel ID and domain verification, and remove unused advanced matching variable.

--- utils/analytics.ts
+++ utils/analytics.ts
@@ -1,19 +1,18 @@
 import ReactPixel from 'react-facebook-pixel';
 
-export const PIXEL_ID = '708580685303880';
+export const PIXEL_ID = '1237712248169466';
 
 let initialized = false;
 
 export const initFacebookPixel = () => {
     if (initialized) return;
 
-    const advancedMatching = {}; // Optional advanced matching data
     const options = {
         autoConfig: true, // set pixel's autoConfig.
         debug: process.env.NODE_ENV === 'development', // enable logs in dev
     };
 
-    ReactPixel.init(PIXEL_ID, advancedMatching, options);
+    ReactPixel.init(PIXEL_ID, undefined, options);
     ReactPixel.pageView();
 
     initialized = true;
